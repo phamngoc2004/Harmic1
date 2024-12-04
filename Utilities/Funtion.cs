2bfa533b784d7fb420d0e17f58f27ce74cb4e32c
Ngọc Phạm Minh <Phamngoc14022004@gmail.com>
2024-12-04 23:44:11 +0700

    up

---
+++ Utilities/Funtion.cs
@@ -0,0 +1,10 @@
+﻿namespace Harmic.Utilities
+{
+    public class Funtion
+    {
+        public static string TitleSlugGenerationAlias(string title)
+        {
+            return SlugGenerator.SlugGenerator.GenerateSlug(title);
+        }
+    }
+}
