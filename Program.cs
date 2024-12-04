88b0f078bbf684ec21539ca333d5e395e6b953e1
Ngọc Phạm Minh <Phamngoc14022004@gmail.com>
2024-12-04 20:46:24 +0700

    up

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using Harmic.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HarmicContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
